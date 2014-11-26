2bcb789cb40d1869936dd97aecf59b35b9be7c8d
Jack <guitarpoet@gmail.com>
2014-11-26 11:39:00 +0800

    Added the clips exec support.

--- src/clips.c
+++ src/clips.c
@@ -11,53 +11,6 @@ static zend_function_entry clips_functions[] = {
     {NULL, NULL, NULL}
 };
 
-
-PHP_FUNCTION(clips_console) {
-	if(p_clips_env) {
-		CommandLoop(p_clips_env);
-	}
-}
-
-PHP_FUNCTION(clips_init) {
-	p_clips_env = CreateEnvironment();
-    RETURN_STRING("Hello World", 1);
-}
-
-PHP_FUNCTION(clips_load) {
-	char* s_filename;
-	int i_filename_len;
-	if(zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &s_filename, &i_filename_len) == FAILURE) {
-		RETURN_FALSE;
-	}
-
-	php_printf("Loading rules file %s", s_filename);
-
-	if(!EnvLoad(p_clips_env, s_filename)) {
-		php_printf("Oh NO!");
-		RETURN_FALSE;
-	}
-	RETURN_TRUE;
-}
-
-PHP_FUNCTION(clips_exec) {
-	if(p_clips_env) {
-		RouterData(p_clips_env)->CommandBufferInputCount = 1;
-		RouterData(p_clips_env)->AwaitingInput = TRUE;
-		EnvAssertString(p_clips_env, "(hello world)\n");
-
-		ExecuteIfCommandComplete(p_clips_env);
-		long start = UNSPECIFIED, end = UNSPECIFIED, max = UNSPECIFIED;
-		struct defmodule *p_module;
-		p_module = ((struct defmodule *) EnvGetCurrentModule(p_clips_env));
-
-		EnvFacts(p_clips_env,WDISPLAY,p_module,start,end,max);
-	}
-}
-
-PHP_FUNCTION(clips_close) {
-    RETURN_STRING("Goodbye World", 1);
-}
-
 zend_module_entry clips_module_entry = {
 #if ZEND_MODULE_API_NO >= 20010901
     STANDARD_MODULE_HEADER,
@@ -74,4 +27,102 @@ zend_module_entry clips_module_entry = {
 #endif
     STANDARD_MODULE_PROPERTIES
 };
+
+
+/*******************************************************************************
+ *
+ *  Function clips_init
+ *
+ *  This function will initialize the clips engine for php
+ *
+ *  @version 1.0
+ *  @args
+ *  	None
+ *
+ *******************************************************************************/
+
+PHP_FUNCTION(clips_init) {
+	p_clips_env = CreateEnvironment();
+	RETURN_TRUE;
+}
+
+/*******************************************************************************
+ *
+ *  Function clips_close
+ *
+ *  This function will close the clips engine
+ *
+ *  @version 1.0
+ *  @args
+ *  	None
+ *
+ *******************************************************************************/
+
+PHP_FUNCTION(clips_close) {
+   	DestroyEnvironment(p_clips_env);
+	p_clips_env = NULL;
+	RETURN_TRUE;
+}
+
+/*******************************************************************************
+ *
+ *  Function clips_console
+ *
+ *  This function will start a command line console for clips
+ *
+ *  @version 1.0
+ *  @args
+ *  	None
+ *
+ *******************************************************************************/
+
+PHP_FUNCTION(clips_console) {
+	if(p_clips_env) {
+		CommandLoop(p_clips_env);
+		RETURN_TRUE;
+	}
+	RETURN_FALSE;
+}
+
+/*******************************************************************************
+ *
+ *  Function clips_exec
+ *
+ *  This function will close the clips engine
+ *
+ *  @version 1.0
+ *  @args
+ *  	filename: The clips file to be executed
+ *
+ *******************************************************************************/
+
+PHP_FUNCTION(clips_exec) {
+	if(p_clips_env) {
+		char* s_filename;
+		int i_filename_len;
+		if(zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &s_filename, &i_filename_len) == FAILURE) {
+			RETURN_FALSE;
+		}
+		EnvBatchStar(p_clips_env, s_filename);
+		RETURN_TRUE;
+	}
+}
+
+PHP_FUNCTION(clips_load) {
+	char* s_filename;
+	int i_filename_len;
+	if(zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &s_filename, &i_filename_len) == FAILURE) {
+		RETURN_FALSE;
+	}
+
+	php_printf("Loading rules file %s", s_filename);
+
+	if(!EnvBatchStar(p_clips_env, s_filename)) {
+		php_printf("Oh NO!");
+		RETURN_FALSE;
+	}
+	RETURN_TRUE;
+}
+
+
 
--- tests/init_test.php
+++ tests/init_test.php
@@ -1,5 +1,5 @@
 <?php
 	echo clips_init();
-	echo clips_load('/tmp/test.rules');
+	echo clips_load(dirname(__FILE__).'/test.rules');
 	echo clips_exec();
 	echo clips_close();
